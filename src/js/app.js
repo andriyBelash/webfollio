6bf46d94d1f2a3769a7a5e1f7e6eb8451fb7076b
andriyBelash <belasha6@gmail.com>
2022-04-12 01:56:52 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import * as flsFunctions from "./modules/functions.js";
+
+// flsFunctions.isWebp();
+flsFunctions.menuInit();
+
+// flsFunctions.isSwiper();
+
+// flsFunctions.isSpoller();
+
+// flsFunctions.isDinamicAdaptive();
+
+// import './files/tippy.js'
+
+// tippy('[data-tippy-content]')
+
+
